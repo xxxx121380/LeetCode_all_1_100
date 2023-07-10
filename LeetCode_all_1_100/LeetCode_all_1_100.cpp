9c5d310408fcb79595257e8f82c7cd007e61d3cb
xxxx121380 <790031275@qq.com>
2023-07-10 14:43:54 +0800

    20230710 14:43

--- LeetCode_all_1_100/LeetCode_all_1_100.cpp
+++ LeetCode_all_1_100/LeetCode_all_1_100.cpp
@@ -21,6 +21,75 @@ public:
                 }
         return temp;
     };
+    //4 寻找两个正序数组的中位数1
+    double findMedianSortedArrays1(vector<int>& nums1, vector<int>& nums2) {
+        int size = nums1.size() + nums2.size();
+        int targetIndex = size / 2;
+        int i = 0;
+        int j = 0;
+        int current=0;
+        int previous=0;
+        while (i + j <= targetIndex) {
+            previous = current;
+            if (i < nums1.size() && (j >= nums2.size() || nums1[i] <= nums2[j])) {
+                current = nums1[i];
+                i++;
+            }
+            else {
+                current = nums2[j];
+                j++;
+            }
+        }
+        double result;
+        if (size % 2 == 0) {
+            result = (previous + current) / 2.0;
+        }
+        else {
+            result = current;
+        }
+        return result;
+    }    
+    //4 寻找两个正序数组的中位数2
+    double findMedianSortedArrays2(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> merged;
+        merged.reserve(nums1.size() + nums2.size());
+        int size = nums1.size() + nums2.size();
+        int i = 0;
+        int j = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (i + j > size / 2)break;
+            if (nums1[i] <= nums2[j]) {
+                merged.push_back(nums1[i]);
+                i++;
+            }
+            else {
+                merged.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while (i < nums1.size()) {
+            if (i + j > size / 2)break;
+            merged.push_back(nums1[i]);
+            i++;
+        }
+        while (j < nums2.size()) {
+            if (i + j > size / 2)break;
+            merged.push_back(nums2[j]);
+            j++;
+        }
+        double result;
+        if (size % 2 == 0) {
+            result = (merged[i + j - 1] + merged[i + j - 2]) / 2.0;
+        }
+        else
+        {
+            result = merged[i + j - 1];
+
+        }
+        return result;
+    }
+
     //16 最接近三数的和
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
@@ -74,7 +143,8 @@ public:
 };
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution solution;
+
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
