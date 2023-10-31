7fcc3997ac45e6e403fc1967129e93294405c6ef
ppy <790031275@qq.com>
2023-10-30 23:57:27 -0700

    complete 100

--- LeetCode_all_1_100/LeetCode_all_1_100.cpp
+++ LeetCode_all_1_100/LeetCode_all_1_100.cpp
@@ -11,6 +11,7 @@
 #include <unordered_map>
 #include <limits>
 #include <stdexcept>
+#include <functional>
 
 using namespace std;
 struct ListNode {
@@ -20,7 +21,14 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode* next) : val(x), next(next) {}   
 };
-
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}    
+};
 class Solution {
 public:
     //1 两数之和
@@ -447,6 +455,13 @@ public:
             head = head->next;
         }
         return node->next;
+    }
+    //100 相同的树
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL) return true;
+        if (p == NULL || q == NULL) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
 int main()
